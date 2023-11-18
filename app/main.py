37370c627eaf414dad0c533956da39a7496db9cb
PALOMAOC <ocanhapcs@gmail.com>
2023-11-18 17:17:36 +0100

    AJUSTE MAIN.PY

--- app/main.py
+++ app/main.py
@@ -1,27 +1,39 @@
 import json
-from google.cloud import storage
-from google.cloud import firestore
+from google.cloud import storage, firestore
 
+# Create instances of the GCS client and Firestore client
+gcs_client = storage.Client()
+firestore_client = firestore.Client()
 
-def cloud_function_handler(data, context):
-    # Obtiene el nombre del bucket y la clave del archivo JSON del evento de GCS
+# Name of the Firestore collection
+collection_name = 'mi-ejercicio-gcp'
+
+def gcs_to_firestore(data, context):
+    # Print the event data
+    print(data)
+
+    # Get the bucket name and object key from the event
     bucket = data['bucket']
     key = data['name']
     print(bucket)
     print(key)
 
-    # Descarga el archivo JSON desde GCS
-    bucket_obj = storage_client.get_bucket(bucket)
+    # Download the JSON file from GCS
+    bucket_obj = gcs_client.bucket(bucket)
     blob = bucket_obj.blob(key)
     json_data = blob.download_as_text()
 
-    # Parsea el JSON
+    # Parse the JSON
     data = json.loads(json_data)
 
-    # Inserta los datos en Firestore
-    db.collection('mi-ejercicio-gcp').document().set({
+    # Insert data into Firestore
+    doc_ref = firestore_client.collection(collection_name).document()
+    doc_ref.set({
         'ID': data['ID'],
         'Nombre': data['Nombre'],
         'Correo electrónico': data['Correo electrónico'],
         'Fecha de registro': data['Fecha de registro']
     })
+
+    return 'Success'
+ 
