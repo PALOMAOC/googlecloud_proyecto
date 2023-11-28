05cf038306833f8b29505cf1a60e080c3008f72e
PALOMAOC <134191936+PALOMAOC@users.noreply.github.com>
2023-11-28 17:40:25 +0100

    Update users.py

--- web/users.py
+++ web/users.py
@@ -30,7 +30,7 @@ app.layout = html.Div([
     html.H1('Navigation Menu'),  # Page title
 
     # Navigation menu
-    dcc.Link('User Form', href='/form'),  # Link to the user form
+    dcc.Link('User Formulario', href='/form'),  # Link to the user form
     html.Br(),  # Line break
     dcc.Link('User Table', href='/user_table'),  # Link to the user table
     html.Br(),  # Line break
